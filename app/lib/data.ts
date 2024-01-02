d2338679784cb3c13e0227005f33c8cd248d7af9
Shubham Chougule <89000326+CosmicStrike@users.noreply.github.com>
2024-01-02 13:24:20 +0530

    remove time out

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,11 +20,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -57,7 +57,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   noStore();
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
 
   try {
     // You can probably combine these into a single SQL query
